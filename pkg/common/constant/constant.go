5989030d5726275e1e4f57f9fd505706f22aef4f
Joakim-animate90 <joakimbwire23@gmail.com>
2024-10-16 18:13:57 +0300

    Add constants for message types, content types, and message queue types

---
+++ pkg/common/constant/constant.go
@@ -0,0 +1,23 @@
+package constant
+
+const (
+	HEAT_BEAT = "heatbeat"
+	PONG      = "pong"
+
+	// Message type, single chat or group chat
+	MESSAGE_TYPE_USER  = 1
+	MESSAGE_TYPE_GROUP = 2
+
+// Message content type
+	TEXT         = 1
+	FILE         = 2
+	IMAGE        = 3
+	AUDIO        = 4
+	VIDEO        = 5
+	AUDIO_ONLINE = 6
+	VIDEO_ONLINE = 7
+
+// Message queue type
+	GO_CHANNEL = "gochannel"
+	KAFKA      = "kafka"
+)
